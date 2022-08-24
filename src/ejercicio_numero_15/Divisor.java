180b3aaaa75e3dd8678d750311c7c3b074063f10
2000LuisM <luis.gonzales@unas.edu.pe>
2022-08-23 22:09:19 -0500

    Codigo Version 1

---
+++ src/ejercicio_numero_15/Divisor.java
@@ -0,0 +1,40 @@
+package ejercicio_numero_15;
+
+import java.util.Scanner;
+
+public class Divisor {
+
+    public static void main(String[] args) {
+        Operacion op1 = new Operacion();
+        op1.leerNumero();
+        op1.calcularDivisores();
+    }
+}
+
+class Operacion {
+
+    int numero;
+
+    public void leerNumero() {
+
+        Scanner leer = new Scanner(System.in);
+        System.out.println("INGRESAR UN NUMERO :");
+        numero = leer.nextInt();
+    }
+
+    public void calcularDivisores() {
+        if (numero != 0 && numero%2 == 0) {
+            for (int i = 1; i <= numero; i++) {
+                if (numero % i == 0) {
+                    //imprimes la solucion
+                    System.out.println("*EL NUMERO " + i + " ES UN DIVISOR DEL " + numero);
+                }
+            }
+
+        } else {
+            System.out.print("'ERROR DE NUMERO'\n");
+
+        }
+    }
+
+}
